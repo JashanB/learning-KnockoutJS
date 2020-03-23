a4d5d74baa7e616c1b9e88c5fa3a86baf510a876
JashanB <jashan_brar50@hotmail.com>
2020-03-23 16:31:04 -0400

    observeables

--- part1.js
+++ part1.js
@@ -1,8 +1,9 @@
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
-  this.firstName = "Bert";
-  this.lastName = "Bertington";
+  this.firstName = ko.observable("Bert");
+  this.lastName = ko.observable("Bertington");
 }
 //binds based on firstName/lastName variables
+//observables - these are properties that automatically will issue notifications whenever their value changes
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
