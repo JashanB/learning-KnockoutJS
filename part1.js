6ca4ebb47a4d40f08f09635edb0255d9fd812644
JashanB <jashan_brar50@hotmail.com>
2020-03-23 16:44:05 -0400

    add computed

--- part1.js
+++ part1.js
@@ -2,6 +2,9 @@
 function AppViewModel() {
   this.firstName = ko.observable("Bert");
   this.lastName = ko.observable("Bertington");
+  this.fullName = ko.computed(function () {
+    return this.firstName() + " " + this.lastName();
+  }, this);
 }
 //binds based on firstName/lastName variables
 //observables - these are properties that automatically will issue notifications whenever their value changes
