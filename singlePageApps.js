dead52b3b1f05062be10b47c4e0d32a88e671399
JashanB <jashan_brar50@hotmail.com>
2020-03-25 14:48:12 -0400

    ajax

---
+++ singlePageApps.js
@@ -0,0 +1,19 @@
+function WebmailViewModel() {
+  // Data
+  var self = this;
+  self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
+  self.chosenFolderId = ko.observable();
+  self.chosenFolderData = ko.observable();
+
+
+  // Behaviours    
+  self.goToFolder = function(folder) { 
+      self.chosenFolderId(folder); 
+      $.get('/mail', { folder: folder }, self.chosenFolderData);
+
+  };     
+  self.goToFolder('Inbox');
+
+};
+
+ko.applyBindings(new WebmailViewModel());
