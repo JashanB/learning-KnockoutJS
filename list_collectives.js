e0786a6f287ded2d65af6a65b453b90913cdd7db
JashanB <jashan_brar50@hotmail.com>
2020-03-25 13:51:14 -0400

    toggle visible

--- list_collectives.js
+++ list_collectives.js
@@ -3,9 +3,9 @@ function SeatReservation(name, initialMeal) {
   var self = this;
   self.name = name;
   self.meal = ko.observable(initialMeal);
-  self.formattedPrice = ko.computed(function() {
-      var price = self.meal().price;
-      return price ? "$" + price.toFixed(2) : "None"
+  self.formattedPrice = ko.computed(function () {
+    var price = self.meal().price;
+    return price ? "$" + price.toFixed(2) : "None"
   })
 }
 // Overall viewmodel for this screen, along with initial state
@@ -14,20 +14,32 @@ function ReservationsViewModel() {
 
   // Non-editable catalog data - would come from the server
 
-  self.addSeat = function() {
+  self.addSeat = function () {
     self.seats.push(new SeatReservation("", self.availableMeals[0]))
-}
+  }
+
+  self.removeSeat = function (seat) {
+    self.seats.remove(seat)
+
+  }
+
+  self.totalSurcharge = ko.computed(function() {
+    var total = 0;
+    for (var i = 0; i < self.seats().length; i++)
+        total += self.seats()[i].meal().price;
+    return total;
+ });
 
   self.availableMeals = [
-      { mealName: "Standard (sandwich)", price: 0 },
-      { mealName: "Premium (lobster)", price: 34.95 },
-      { mealName: "Ultimate (whole zebra)", price: 290 }
-  ];    
+    { mealName: "Standard (sandwich)", price: 0 },
+    { mealName: "Premium (lobster)", price: 34.95 },
+    { mealName: "Ultimate (whole zebra)", price: 290 }
+  ];
 
   // Editable data
   self.seats = ko.observableArray([
-      new SeatReservation("Steve", self.availableMeals[0]),
-      new SeatReservation("Bert", self.availableMeals[0])
+    new SeatReservation("Steve", self.availableMeals[0]),
+    new SeatReservation("Bert", self.availableMeals[0])
   ]);
 }
 
